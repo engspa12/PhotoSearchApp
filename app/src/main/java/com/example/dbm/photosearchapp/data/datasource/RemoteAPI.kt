9d9a825052954f881238525896b52b00705431dd
Daniel Bedoya <arturo.lpc12@gmail.com>
2022-10-28 23:33:22 -0500

    Initial commit

---
+++ app/src/main/java/com/example/dbm/photosearchapp/data/datasource/RemoteAPI.kt
@@ -0,0 +1,27 @@
+package com.example.dbm.photosearchapp.data.datasource
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RemoteAPI {
+
+    /*@GET("rest/?method=flickr.interestingness.getList")
+    suspend fun getPhotosFromFeed(
+        @Query("api_key") apiKey: String = Constants.API_KEY_VALUE,
+        @Query("per_page") perPage: Int = Constants.PER_PAGE_VALUE,
+        @Query("format") format: String = Constants.FORMAT_VALUE,
+        @Query("extras") extras: String = Constants.EXTRAS_VALUE,
+        @Query("nojsoncallback") nojsoncallback: String = Constants.NO_JSON_CALLBACK_VALUE
+    ): FlickrPhotosResponse
+
+    @GET("rest/?method=flickr.photos.search")
+    suspend fun getPhotosBySearchTerm(
+        @Query("api_key") apiKey: String = Constants.API_KEY_VALUE,
+        @Query("text") searchTerm: String,
+        @Query("sort") sort: String = Constants.SORT_VALUE,
+        @Query("per_page") perPage: Int = Constants.PER_PAGE_VALUE,
+        @Query("format") format: String = Constants.FORMAT_VALUE,
+        @Query("extras") extras: String = Constants.EXTRAS_VALUE,
+        @Query("nojsoncallback") nojsoncallback: String = Constants.NO_JSON_CALLBACK_VALUE
+    ): FlickrPhotosResponse*/
+}
---
+++ app/src/main/java/com/example/dbm/photosearchapp/presentation/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dbm.photosearchapp.presentation.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dbm.photosearchapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
